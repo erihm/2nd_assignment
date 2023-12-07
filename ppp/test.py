5f9dae15ba78e597ae179209c0abc479875e8ae4
Erika Magdalena Herrera Machado machado <machado@sdu-134020.c.sdu.dk>
2023-12-07 11:00:06 +0100

    Changes after first meeting

--- ppp/test.py
+++ ppp/test.py
@@ -14,51 +14,63 @@ enzymes = powerset(inputRules)[1:]
 good_enzymes = []
 bad_enzymes = []
 
+# Use the same dg for all the solutions
+# Remove the try-remove 
+# The objective function is correct
+# To access the values of the flow solution use eval()
+# Visualize lattice with values 
+# Add an explanation for the subsets that worked
+# Use another chemistry: knock?
+# Design a system w. artificial rules for illustration
+# "Waste" is the amount of carbons
+# Does the quality improve with more enzymes? Is the flow more complex w. more enzymes?
+# W. more enzymes the flow might be shorter.
 
 for set in enzymes:
     inputRules = list(set)
 
-    try: 
-        # This part is from dg.py
-        pShortest = lambda d: all(a.vLabelCount("C") <= 8 for a in d.right) # WHY
+    # This part is from dg.py
+    pShortest = lambda d: all(a.vLabelCount("C") <= 8 for a in d.right) # The likelihood of a big molecule undergoing a reaction is very high
 
-        strat = (
-            addSubset(ribuloseP, water)
-            >> rightPredicate[pShortest]( # put pShorter, pEvenShorter or pShortest here, instead of p (they are all equivalent)
-                    repeat(inputRules)
-            )
+    strat = (
+        addSubset(ribuloseP, water)
+        >> rightPredicate[pShortest]( # put pShorter, pEvenShorter or pShortest here, instead of p (they are all equivalent)
+                repeat(inputRules)
         )
-        dg = DG(graphDatabase=inputGraphs)
-        dg.build().execute(strat)
+    )
+    dg = DG(graphDatabase=inputGraphs)
+    dg.build().execute(strat)
 
-        # This is from pathway.py
-        flow = Flow(dg)
-        flow.addSource(ribuloseP)
-        flow.addSource(water)
-        # flow.addConstraint(inFlow[ribuloseP] == 6)
-        # flow.addConstraint(inFlow[water] == 1)
-        flow.addSink(fructoseP)
-        flow.addConstraint(outFlow[fructoseP] >= 1)
-        flow.objectiveFunction = -outFlow[fructoseP]
-        for v in dg.vertices: flow.addSink(v.graph)
-        flow.findSolutions(verbosity=0)
-        print("For this set of enzymes: ", inputRules)
-        flow.solutions.list()
-        good_enzymes.append(inputRules)
+    # This is from pathway.py
+    flow = Flow(dg)
+    flow.addSource(ribuloseP)
+    flow.addSource(water)
+    flow.addConstraint(inFlow[ribuloseP] == 60) # ideal would be to get 50 of fructose
+    # flow.addConstraint(inFlow[water] == 1)
+    flow.addSink(fructoseP)
+    flow.addConstraint(outFlow[fructoseP] >= 1)
+    flow.objectiveFunction = -outFlow[fructoseP] # dg.findVertex(fructoseP)
+    for v in dg.vertices: flow.addSink(v)
+    flow.findSolutions(verbosity=0, absGap=0, maxNumSolution=2**20)
+    
+    # while True: 
+    #   sols = flow.findSolution()
+    #   if jfiohfaohaio:
+    #   break
+    
+    
+    print("For this set of enzymes: ", inputRules)
+    flow.solutions.list()
+    good_enzymes.append(inputRules)
 
-        print("For this set of enzymes we obtained %s solution/s" % len(flow.solutions))
+    print("For this set of enzymes we obtained %s solution/s" % len(flow.solutions))
+    
+    postSection("Enzymes: %s " % inputRules)
+    flowPrinter = FlowPrinter()
+    flowPrinter.printUnfiltered = False
+    flow.solutions.print()
         
-        postSection("Enzymes: %s " % inputRules)
-        flowPrinter = FlowPrinter()
-        flowPrinter.printUnfiltered = False
-        flow.solutions.print()
-        
-        del dg
-        del flow
-        del inputRules
-        del strat
-        
-    except:
+    if (len(flow.solutions) == 0):
         bad_enzymes.append(inputRules)
         continue
     
@@ -66,7 +78,8 @@ for x in good_enzymes:
     print(x)
     
 # First solution
-# s0 = flow.solutions[0]
+s0 = flow.solutions[0]
+s0.eval(outFlow[fructoseP])
 # Print info about the solution
 # s0.list()
 # Flow for this solution
